1b5bf9072dc73a437c6570fb24dabd10369e3324
Chesnok <d.chesnokov20@gmail.com>
2023-11-20 17:49:43 +0300

    Init commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RussianLangTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Excercise> Tasks { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tasks = new List<Excercise>();
+            
+        }
+
+        private void GenButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Tasks.Add(new Excercise(i + 1, "Тест", "Тест", "Тест"));
+            }
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        public static void GenerateField(int rows)
+        {
+            
+        }
+    }
+    
+}
---
+++ Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel.Design;
+
+namespace RussianLangTest
+{
+
+    public class Excercise
+    {
+        public string? row;
+        public string? task;
+        public string? answer;
+        public string? correct;
+        public Excercise(int row, string task, string answer, string correct)
+        {
+            this.row = row.ToString();
+            this.task = task;
+            this.answer = answer;
+            this.correct = correct;
+        }
+
+        public Excercise()
+        {
+
+        }
+    }
+
+    
+}
+
+    
