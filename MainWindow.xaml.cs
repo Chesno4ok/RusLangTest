1f0dda44ee294d6de4bac4f2d7abd54a6be29678
Chesnok <d.chesnokov20@gmail.com>
2023-11-24 14:20:53 +0300

    final

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,24 +22,70 @@ namespace RussianLangTest
     public partial class MainWindow : Window
     {
         public List<Excercise> Tasks { get; set; }
+        public DateTime start;
         public MainWindow()
         {
             InitializeComponent();
+
             Tasks = new List<Excercise>();
-            
+            TableManager.RefreshTable(table);
         }
 
         private void GenButton_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 5; i++)
+            
+            
+            int rows = 0;
+
+            if (!Int32.TryParse(RowInput.Text, out rows))
             {
-                Tasks.Add(new Excercise(i + 1, "Тест", "Тест", "Тест"));
+                return;
             }
+            if (rows < 0)
+            {
+                return;
+            }
+
+            
+
+            start = DateTime.Now;
+            string rx = GenerateRx();
+            if(rx == null)
+            {
+                return;
+            }
+
+            Tasks.Clear();
+            Tasks = TableManager.GenerateTasks(new Regex(rx), rows);
+
+
+            TableManager.FillTable(table, Tasks);
+            CheckButton.IsEnabled = true;
         }
 
         private void CheckButton_Click(object sender, RoutedEventArgs e)
         {
+            int correct = 0;
+            
+            foreach(Excercise i in Tasks)
+            {
+                i.correct.Visibility = Visibility.Visible;
+                i.answer.IsEnabled = false;
+                CheckButton.IsEnabled = false;
 
+                if(i.answer.Text == i.correct.Text)
+                {
+                    i.correct.Background = new SolidColorBrush(Colors.Green);
+                    correct++;
+                }
+                else
+                {
+                    i.correct.Background = new SolidColorBrush(Colors.Red);
+                }
+
+            }
+
+            MessageBox.Show($"Вы выполнили {correct} заданий из {Tasks.Count} \n Это заняло {(DateTime.Now - start).Minutes} минут, {(DateTime.Now - start).Seconds} секунд", "Вы выполнили задание!");
         }
 
         private void DataGrid_Loaded(object sender, RoutedEventArgs e)
@@ -48,6 +95,43 @@ namespace RussianLangTest
         public static void GenerateField(int rows)
         {
             
+        }
+
+        public string GenerateRx()
+        {
+            string? rx = @"";
+
+            if((bool)RB_1.IsChecked)
+            {
+                rx += "|безударная гласная";
+            }
+            if ((bool)RB_2.IsChecked)
+            {
+                rx += "|словарное слово";
+            }
+            if ((bool)RB_3.IsChecked)
+            {
+                rx += "|окончания склонений";
+            }
+            if ((bool)RB_4.IsChecked)
+            {
+                rx += "|глухие согласные";
+            }
+
+            if(rx.Length > 0)
+            {
+                if (rx[0] == '|')
+                {
+                    rx = rx.Remove(0, 1);
+                }
+            }
+            else
+            {
+                rx = null;
+            }
+            
+
+            return rx;
         }
     }
     
--- Task.cs
+++ Task.cs
@@ -8,27 +8,120 @@ using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Controls.Primitives;
 using System.ComponentModel.Design;
+using System.Text.RegularExpressions;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.IO;
 
 namespace RussianLangTest
 {
 
     public class Excercise
     {
-        public string? row;
-        public string? task;
-        public string? answer;
-        public string? correct;
-        public Excercise(int row, string task, string answer, string correct)
+        public TextBlock row = new TextBlock();
+        public TextBlock task = new TextBlock();
+        public TextBox answer = new TextBox();
+        public TextBlock correct = new TextBlock();
+
+
+        public Excercise(int row, string task, string correct)
         {
-            this.row = row.ToString();
-            this.task = task;
-            this.answer = answer;
-            this.correct = correct;
+            this.row.Text = row.ToString();
+            this.task.Text = task;
+            this.correct.Text = correct;
+            this.correct.Visibility = Visibility.Hidden;
+
+            this.task.TextWrapping = TextWrapping.Wrap;
+        }
+    }
+
+    public class Root
+    {
+        public List<Task> Tasks { get; set; }
+    }
+
+    public class Task
+    {
+        public string task { get; set; }
+        public string type { get; set; }
+        public string correct { get; set; }
+    }
+
+
+    public static class TableManager
+    {
+        public static void FillTable(UniformGrid table, List<Excercise> tasks)
+        {
+            RefreshTable(table);
+
+            foreach(Excercise i in tasks)
+            {
+                table.Children.Add(i.row);
+                table.Children.Add(i.task);
+                table.Children.Add(i.answer);
+                table.Children.Add(i.correct);
+            }
         }
 
-        public Excercise()
+        public static void RefreshTable(UniformGrid table)
         {
+            table.Children.Clear();
 
+            var id = new TextBlock();
+            id.Text = "ID";
+            table.Children.Add(id);
+
+            var task = new TextBlock();
+            task.Text = "Задание";
+            table.Children.Add(task);
+
+            var answer = new TextBlock();
+            answer.Text = "Ответ";
+            table.Children.Add(answer);
+
+            var correct = new TextBlock();
+            correct.Text = "Правильный ответ";
+            table.Children.Add(correct);
+        }
+        public static List<Excercise> GenerateTasks(Regex rx, int rows)
+        {
+            List<Excercise> res = new List<Excercise>();
+
+            Root? value = (Root?)JsonSerializer.Deserialize(File.ReadAllText("tasks.json"), typeof(Root));
+            
+            List<Task> lst = value.Tasks.Where(i => rx.IsMatch(i.type)).ToList();
+            Shuffle(lst);
+
+            int x = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                if(x > lst.Count - 1)
+                {
+                    Shuffle(lst);
+                    x = 0;
+                }
+
+                res.Add(new Excercise(i + 1, lst[x].task, lst[x].correct));
+
+                x++;
+            }
+
+                
+            return res;
+        }
+
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 
